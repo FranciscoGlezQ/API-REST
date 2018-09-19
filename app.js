3cdd3b0843990c43d1e7c71105415e21362ec1e9
Francisco <ciscogonzalezquintero@outlook.com>
2018-09-19 17:51:46 -0500

    API Rest with node

---
+++ app.js
@@ -0,0 +1,23 @@
+import express from "express"
+import bodyParser from "body-parser"
+import db from "./db/db"
+
+import router from './routes/index.js'
+
+//Set up the express app
+const app = express()
+
+//Parse incoming request data
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(router)
+
+//get all to dos
+
+
+
+const PORT = 5000
+
+app.listen(PORT, () =>{
+	console.log('server running on port '+PORT)
+})
